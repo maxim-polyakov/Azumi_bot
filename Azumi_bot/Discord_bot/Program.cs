e648508835421a0fe44f18a97e3ec9f914d950a5
maxim-polyakov <maxim7012@mail.ru>
2022-11-30 23:10:09 +0300

    Azumi

---
+++ Azumi_bot/Discord_bot/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    private DiscordSocketClient _client;
+    
+ //   public static Task MainAsync()
+ //   {
+       // _client = new DiscordSocketClient();
+       // _client.Log += Log;
+       // var token = "token";
+       // await _client.LoginAsync(TokenType.Bot, token);
+      //  await _client.StartAsync();
+      //  await Task.Delay(-1);
+  //  }
+}
---
+++ Telegram_Bot/Telegram_Bot/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+
+namespace TelegramBotExperiments
+{
+
+    class Program
+    {
+        static ITelegramBotClient bot = new TelegramBotClient("5652073179:AAGYa1IDz4BkVtSdYC22NVYnaodLXK3haRQ");
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            if(update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var message = update.Message;
+
+                await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
+            }
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, // receive all update types
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
+        }
+    }
+}
