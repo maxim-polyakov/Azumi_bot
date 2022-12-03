58b6731a049863958cf3c48d8d9b0ee7e3228ae9
maxim-polyakov <maxim7012@mail.ru>
2022-12-03 15:30:16 +0300

    discord_bot api

--- Azumi_bot/Discord_bot/Program.cs
+++ Azumi_bot/Discord_bot/Program.cs
@@ -1,30 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using Discord;
-using Discord.Commands;
 using Discord.WebSocket;
 
-public class Program
+namespace Discord_bot
 {
-     static DiscordSocketClient _client;
-     static BaseSocketClient _baseSocketClient;
-     static Task ditme(LogMessage msg) 
-     { 
-         Console.WriteLine(msg.ToString());
-         return Task.CompletedTask;
-     }
-    
-     public static async  Task Main()
-     {
-         _baseSocketClient = new DiscordShardedClient();
-         _client = new DiscordSocketClient();
-         _client.Log += ditme;
-         _baseSocketClient.Log += ditme;
-         var token = "MTA0ODQ5NTY4MDQ3OTU4MDIzMA.GOH65o.juuA4O3T_O0_fnBJIKUztcxE7FPwLiwy6x3r3c";
-         await _client.LoginAsync(TokenType.Bot, token);
-         await _client.StartAsync();
-         await Task.Delay(-1);
-     }
+    class Program
+    {
+        private DiscordSocketClient client = new DiscordSocketClient();
+
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            client.MessageReceived += CommandHandler;
+
+            var token = "MTA0ODQ5NTY4MDQ3OTU4MDIzMA.GhQ3T5.i3cG1U9wOm6etYEfo2VfvNqE9zbl1zqZcQAWEo";
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            Console.ReadLine();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task CommandHandler(SocketMessage msg)
+        {
+            if (!msg.Author.IsBot)
+            {
+                switch (msg.Content)
+                {
+                    case "!привет":
+                        msg.Channel.SendMessageAsync($"Привет,  {msg.Timestamp}");
+                        break;
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
 }
