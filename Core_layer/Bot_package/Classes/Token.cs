9d6de0e634fa85a813abcf8a57502aa4e38641c9
maxim-polyakov <maxim7012@mail.ru>
2023-02-18 23:12:23 +0300

    Names_fixes

--- Core_layer/Bot_package/Classes/Token.cs
+++ Core_layer/Bot_package/Classes/Token.cs
@@ -1,7 +1,24 @@
-﻿namespace Bot_package
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB_package;
+
+namespace Bot_package
 {
-    public class Token:AToken
+    public class Token:IToken
     {
-        public string get_token(string select) => base.get_token(select);
+        private static IDB_Communication bridge = new DB_Communication();
+
+        public string get_token(string select)
+        {
+            return bridge.get_token(select);
+        }
+
+        public void add_token(string token)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
