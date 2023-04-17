d782f9be6310d49ae58f72e3e856d6e807554503
maxim-polyakov <maxim7012@mail.ru>
2023-04-17 20:16:04 +0300

    The last changes

--- Core_layer/Test_package/NLP_package_Predictors_Test.cs
+++ Core_layer/Test_package/NLP_package_Predictors_Test.cs
@@ -8,3 +8,20 @@ using Microsoft.Data.Analysis;
 using System.Threading;
 using static System.Net.Mime.MediaTypeNames;
 
+[TestClass]
+public class Predictor_Test
+{
+    static IPredictor predictor = new Predictor();
+
+    [TestMethod]
+    public void TestMethod_Predict()
+    {
+        Maps map = new Maps();
+
+        string text = "ïðèâåò";
+        var hiflag = predictor.predict(text, "C:\\Users\\maxim\\Documents\\GitHub\\Azumi_bot\\NLP_package\\Models\\himodel.zip", map.himap);
+        var thflag = predictor.predict(text, "C:\\Users\\maxim\\Documents\\GitHub\\Azumi_bot\\NLP_package\\Models\\thmodel.zip", map.thmap);
+        var businessflag = predictor.predict(text, "C:\\Users\\maxim\\Documents\\GitHub\\Azumi_bot\\NLP_package\\Models\\businessmodel.zip", map.businessmap);
+        var trashflag = predictor.predict(text, "C:\\Users\\maxim\\Documents\\GitHub\\Azumi_bot\\NLP_package\\Models\\trashmodel.zip", map.trashmap);
+    }
+}
