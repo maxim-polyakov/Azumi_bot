f4c01fdb7ea5dc53e2cc13269270c4b1d6e732d7
maxim-polyakov <maxim7012@mail.ru>
2023-07-11 02:13:47 +0300

    123

--- Deep_layer/NLP_package/Classes/Gpt.cs
+++ Deep_layer/NLP_package/Classes/Gpt.cs
@@ -7,9 +7,10 @@ namespace NLP_package
     {
         public string generate(string text)
         {
-            var api = new OpenAI_API.OpenAIAPI("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");
-            var result = api.Completions.GetCompletion(text);
-            return result.Result.ToString();
+            //var api = new OpenAI_API.OpenAIAPI("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");
+            //var result = api.Completions.GetCompletion(text);
+            //return result.Result.ToString();
+            return string.Empty;
         }
     }
 }
