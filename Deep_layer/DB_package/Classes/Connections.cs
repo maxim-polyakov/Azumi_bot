b312358e7463591d5f54317424691c80c306c870
maxim-polyakov <maxim7012@mail.ru>
2024-06-23 13:36:55 +0300

    1

--- Deep_layer/DB_package/Classes/Connections.cs
+++ Deep_layer/DB_package/Classes/Connections.cs
@@ -5,7 +5,7 @@
 
         public Connections()
         {
-            this.cs_remote = "Host=ep-round-paper-091468.us-east-2.aws.neon.tech;Username=postgres;Password=gaTResKPJX25;Database=SistersMemory";
+            this.cs_remote = "Host=ep-round-paper-091468.us-east-2.aws.neon.tech;Username=postgres;Password=gaTResKPJX25;Database=BotsMemory";
         }
     }
 }
